027a479f7a1da2f1fb27aa788a5de02dd1d951ec
Seifbarouni <seif.barouni11@gmail.com>
2022-01-16 11:50:58 +0100

    added main.go in cmd folder

--- internal/createConsole/create-console.go
+++ internal/createConsole/create-console.go
@@ -41,6 +41,21 @@ func CreateConsoleApp(folderName string) {
 	h.AddFolderAndReadme("cmd", "# `/cmd`\n This folder contains the main.go file of the app")
 	h.AddFolderAndReadme("internal", "# `/internal`\n Private application and library code. This is the code you don't want others importing in their applications or libraries.")
 	h.AddFolderAndReadme("pkg", "# `/pkg`\n Library code that's ok to use by external applications.")
+	// go to cmd folder with the os package
+	err := os.Chdir("cmd")
+	if err != nil {
+		colorize.PrintWithColor("Error moving to folder", colorize.Red)
+		os.Exit(1)
+	}
+	// create the main.go file
+	h.CreateFile("main.go", "package main\n\nimport \"fmt\"\n\nfunc main() {\n\tfmt.Println(\"Hello World!\")\n}")
+
+	// go back to the root folder
+	err = os.Chdir("..")
+	if err != nil {
+		colorize.PrintWithColor("Error moving to folder", colorize.Red)
+		os.Exit(1)
+	}
 
 	h.ExecuteCommand("git", "init")
 
