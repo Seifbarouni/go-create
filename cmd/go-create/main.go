9b8eb3a1d5ac8f659c16536eeb83b451fcfa2542
Seifbarouni <seif.barouni11@gmail.com>
2022-01-27 20:47:05 +0100

    main.go cleanup

--- cmd/go-create/main.go
+++ cmd/go-create/main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	cmd "github.com/Seifbarouni/go-create/cmd/cobra"
-	/* "fmt"
-	"os"
-
-	console "github.com/Seifbarouni/go-create/internal/createConsole"
-	db "github.com/Seifbarouni/go-create/internal/createDB"
-	md "github.com/Seifbarouni/go-create/internal/createModel"
-	sv "github.com/Seifbarouni/go-create/internal/createService"
-	web "github.com/Seifbarouni/go-create/internal/createWeb" */)
+import cmd "github.com/Seifbarouni/go-create/cmd/cobra"
 
 func main() {
-/* 	//check if the user has provided 3 arguments
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: go-create <app-type> <folder-name>\nExample: go-create web .\nAvailable app types: web, cli\nOther functionalities: db <file-name>, service <concerned-model>, model <file-name>")
-		os.Exit(1)
-	}
-	// get the app type and path
-	appType := os.Args[1]
-	folderOrFileName := os.Args[2]
-
-	// check if the app type is valid with a switch statement
-	switch appType {
-	case "web":
-		web.CreateWebApp(folderOrFileName)
-	case "cli":
-		console.CreateConsoleApp(folderOrFileName)
-	case "db":
-		db.CreateDB(folderOrFileName)
-	case "model":
-		md.CreateModel(folderOrFileName)
-	case "service":
-		sv.CreateService(folderOrFileName)
-	default:
-		fmt.Println("Invalid app type\nAvailable types: web, cli, db, service, model")
-		os.Exit(1)
-	} */
 	cmd.Execute()
 }
